197ce7b6e24e6b676111edd0835b438a82c91d1c
masprog <maurolove09@gmail.com>
2021-07-14 16:41:51 -0700

    finishing the app

--- app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
+++ app/src/main/java/com/example/caraoucoroa/ResultadoActivity.java
@@ -3,12 +3,40 @@ package com.example.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private ImageView imageResultado;
+    private ImageView btnVoltar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        imageResultado = findViewById(R.id.imageResultado);
+        btnVoltar = findViewById(R.id.btnVoltar);
+
+
+
+        //recuperar dados
+        Bundle dados = getIntent().getExtras();
+        int numero = dados.getInt("numero");
+
+        if (numero == 0 ){ // cara
+            imageResultado.setImageResource(R.drawable.moeda_cara);
+        }else{ // coroa
+            imageResultado.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
